913bd54bc430601362441aa3ca1bc895b176a12a
paul <paul@driftingin.space>
2023-05-15 22:44:07 -0400

    progress

--- demos/stable-diffusion/server/main.py
+++ demos/stable-diffusion/server/main.py
@@ -1,140 +1,124 @@
-import asyncio
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+from diffusers.loaders import TextualInversionLoaderMixin
+from diffusers.utils import randn_tensor
 from typing import Optional
+from threading import Thread
+import io
 
 from wirebind.rpc import expose
 from wirebind.binds.atom import Atom
 from wirebind.sender import Sender
 
 
-class Slider:
-    value = Atom(50)
+MODEL_ID = "stabilityai/stable-diffusion-2"
+
+class StableDiffusion:
+    prompt = Atom("two dogs playing tug with a french baguette")
+    result = Atom(None)
+    thread: Optional[Thread] = None
+    pipe: StableDiffusionPipeline
+    latents: torch.Tensor
+    prompt_embeds: torch.Tensor
+    num_timesteps: int = 50
+    current_timestep: int = 0
+
+    def prepare_latents(self):
+        self.latents = self.pipe.prepare_latents(
+            1,
+            self.pipe.unet.config.in_channels,
+            self.pipe.unet.config.sample_size * self.pipe.vae_scale_factor,
+            self.pipe.unet.config.sample_size * self.pipe.vae_scale_factor,
+            torch.float16,
+            self.pipe._execution_device,
+            None,
+            None,
+        )
 
 
-class Timer:
-    value = Atom(0)
-    task: Optional[asyncio.Task]
+    def prepare_prompt_embeds(self):
+        self.prompt_embeds = self.pipe._encode_prompt(
+            "three cats", #self.prompt.get(),
+            self.pipe._execution_device,
+            1,
+            True,
+        )
+
 
     def __init__(self):
-        self.task = None
+        self.pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16)
+        self.pipe.to("cuda")
+        self.prepare_latents()
+        self.prepare_prompt_embeds()
 
-    @expose("start")
-    def start(self):
-        if self.task is not None:
-            return
-        self.task = asyncio.create_task(self.incr())
-
-    @expose("stop")
-    def stop(self):
-        self.task.cancel()
-        self.task = None
-
-    @expose("reset")
-    def reset(self):
-        self.value.set(0)
-
-    async def incr(self):
-        while True:
-            await asyncio.sleep(0.1)
-            self.value.set(self.value.get() + 1)
+        self.thread = Thread(target=self.run_diffusion)
+        self.thread.start()
 
 
-class Counter:
-    def __init__(self):
-        self.count = Atom(0)
-
-    @expose("inc")
-    def inc(self):
-        self.count.set(self.count.get() + 1)
-
-    @expose("dec")
-    def dec(self):
-        self.count.set(self.count.get() - 1)
+    def latents_to_image(self):
+        latents = 1 / self.pipe.vae.config.scaling_factor * self.latents
+        image = self.pipe.vae.decode(latents).sample
+        image = (image / 2 + 0.5).clamp(0, 1)
+        # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
+        image = image.cpu().detach().permute(0, 2, 3, 1).float().numpy()
+        image, _ = self.pipe.run_safety_checker(image, 'cuda', self.prompt_embeds.dtype)
+        image = self.pipe.numpy_to_pil(image)
+        return image[0]
 
 
-class Calc:
-    hidden_register = None
-    operation = None
-    next_action_clears = False
-    display = Atom(0)
-
-    def op(self):
-        if self.hidden_register is None:
-            raise Exception("No hidden register")
-        elif self.operation is None:
-            raise Exception("No operation")
-        elif self.operation == "+":
-            self.display.set(self.hidden_register + self.display.get())
-        elif self.operation == "-":
-            self.display.set(self.hidden_register - self.display.get())
-        elif self.operation == "*":
-            self.display.set(self.hidden_register * self.display.get())
-        elif self.operation == "/":
-            self.display.set(self.hidden_register / self.display.get())
-        else:
-            raise Exception("Unknown operation")
-        self.hidden_register = None
-        self.operation = None
-
-    @expose("clear")
-    def press_clear(self):
-        self.display.set(0)
-        self.hidden_register = None
-        self.operation = None
-
-    @expose("digit")
-    def press_digit(self, digit: int):
-        if self.next_action_clears:
-            self.display.set(0)
-            self.next_action_clears = False
-        if self.display.get() == 0:
-            self.display.set(digit)
-        else:
-            self.display.set(self.display.get() * 10 + digit)
-
-    @expose("op")
-    def press_operation(self, op: str):
-        if op not in ["+", "-", "*", "/"]:
-            raise Exception(f"Unknown operation {op}")
-        self.hidden_register = self.display.get()
-        self.operation = op
-        self.next_action_clears = True
-
-    @expose("eq")
-    def press_eq(self):
-        self.op()
-        self.hidden_register = None
-        self.operation = None
-        self.next_action_clears = True
+    def update_image(self):
+        image = self.latents_to_image()
+        bytes = io.BytesIO()
+        image.save(bytes, format='jpeg')
+        self.result.set(bytes.getvalue())
 
 
-GLOBAL_STATE = {
-    "slider": Slider(),
-    "timer": Timer(),
-    "counter": Counter(),
-    "calc": Calc(),
-}
+    @torch.no_grad()
+    def run_diffusion(self):
+        self.pipe.scheduler.set_timesteps(50)
+        timesteps = self.pipe.scheduler.timesteps
+        prompt_embeds = self.prompt_embeds
+
+        guidance_scale = 7.5
+        do_classifier_free_guidance = True
+
+        extra_step_kwargs = self.pipe.prepare_extra_step_kwargs(None, 0.0)
+
+        for i, t in enumerate(timesteps):
+            print(i)
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = torch.cat([self.latents] * 2) if do_classifier_free_guidance else self.latents
+            latent_model_input = self.pipe.scheduler.scale_model_input(latent_model_input, t)
+
+            # predict the noise residual
+            noise_pred = self.pipe.unet(
+                latent_model_input,
+                t,
+                encoder_hidden_states=prompt_embeds,
+                return_dict=False,
+            )[0]
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            # # compute the previous noisy sample x_t -> x_t-1
+            self.latents = self.pipe.scheduler.step(noise_pred, t, self.latents, **extra_step_kwargs, return_dict=False)[0]
+
+            if i % 10 == 0:
+               self.update_image()
+
+
+STABLE_DIFFUSION = StableDiffusion()
 
 
 def root(message: any):
-    typ = message["typ"]
     reply = message["reply"]
 
-    if typ in GLOBAL_STATE:
-        obj = GLOBAL_STATE[typ]
-        # Iterate over the fields in obj, and send the ones that implement
-        # Packable to the client.
-        result = {}
-
-        for field_name in dir(obj):
-            field = getattr(obj, field_name)
-            if isinstance(field, Atom):
-                result[field_name] = field
-
-            # If the field is a method check if it is exposed and if so
-            # add it to the result.
-            if hasattr(field, "_exposed_as"):
-                result[field._exposed_as] = Sender(
-                    lambda args, field=field: field(**(args or {}))
-                )
-
-        reply.send(result)
+    result = {
+        "prompt": STABLE_DIFFUSION.prompt,
+        "result": STABLE_DIFFUSION.result,
+    }
+    
+    reply.send(result)
--- demos/stable-diffusion/src/app/page.tsx
+++ demos/stable-diffusion/src/app/page.tsx
@@ -1,20 +1,20 @@
 "use client"
 
-import BigButton from "@/components/BigButton"
 import { RemoteProvider, useRemoteObject, useRemoteValue } from "@/lib/remote"
 
 function Counter() {
-  const counter = useRemoteObject("counter")
-  const value = useRemoteValue(counter?.count)
+  const sdApp = useRemoteObject("stable-diffusion")
+  const imageData = useRemoteValue(sdApp?.result)
   
+  let url = null
+  if (imageData) {
+    const blob = new Blob([imageData], { type: 'image/jpeg' });
+    url = URL.createObjectURL(blob);
+  }
+
   return (
     <div className="flex flex-col space-y-3">
-      <div className="text-4xl text-center">{value ?? <span className="text-gray-300">0</span>}</div>
-
-      <div className="flex space-x-2">
-        <BigButton onClick={() => counter?.inc.send()}>+</BigButton>
-        <BigButton onClick={() => counter?.dec.send()}>-</BigButton>
-      </div>
+      { url ? <img src={url} /> : null }
     </div>
   )
 }

7a9e16a654df0555f1f87779289f53435ef10bb7
paul <paul@driftingin.space>
2023-05-16 07:33:17 -0400

    styles

--- demos/stable-diffusion/src/app/page.tsx
+++ demos/stable-diffusion/src/app/page.tsx
@@ -6,7 +6,7 @@ function Counter() {
   const sdApp = useRemoteObject("stable-diffusion")
   const imageData = useRemoteValue(sdApp?.result)
   const [prompt, setPrompt] = useRemoteMutable(sdApp?.prompt)
-  
+
   let url = null
   if (imageData) {
     const blob = new Blob([imageData], { type: 'image/jpeg' });
@@ -15,9 +15,11 @@ function Counter() {
 
   return (
     <div className="flex flex-col space-y-3">
-      { url ? <img src={url} /> : null }
+      {url ? <img src={url} /> : null}
 
-      <input type="text" value={prompt} onChange={(t) => setPrompt(t.target.value)} />
+      <input type="text" value={prompt} onChange={(t) => setPrompt(t.target.value)}
+        className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
     </div>
   )
 }
